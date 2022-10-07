d8a1bc7534abb365819b695c19e3129fef77d57d
Vinay Pant <vinaypant24@gmail.com>
2022-10-07 13:06:09 +0530

    Initial Commit files created and no functionality is been added

---
+++ Front_End/scripts/firstpage.js
@@ -0,0 +1,7 @@
+// document.getElementById('clickme2').onclick = function(){
+//     eel.maindata("Calling from JS and getting the data soon")(function(ret){alert(ret)})
+// }
+
+// File responsible for the sub function running on the main page. 
+
+
---
+++ Time_Manager.py
@@ -0,0 +1,12 @@
+""" Main file for the UI and is responsible for running the main application """
+
+
+# import eel
+# eel.init('web')
+
+# @eel.expose
+# def maindata(dummy_parm):
+#     print(dummy_parm)
+#     return dummy_parm
+
+# eel.start('index.html' , size=(20,20))
